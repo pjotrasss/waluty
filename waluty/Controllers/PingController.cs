55f0d0e709baf2be811f2fdfd7e3471074ebd4e0
pjotrasss <pjotrasss@dom>
2025-08-10 22:58:23 +0200

    pingpong

---
+++ waluty/Controllers/PingController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace waluty.Controllers {
+    [ApiController]
+    [Route("/ping")]
+    public class PingController : ControllerBase {
+        [HttpGet]
+        public string Get() => "pong";
+    }
+}
--- waluty/Program.cs
+++ waluty/Program.cs
@@ -1,25 +1,11 @@
+//variable for preparing app
 var builder = WebApplication.CreateBuilder(args);
+//add services to app
+    builder.Services.AddControllers();
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
+//building app
 var app = builder.Build();
+    app.MapControllers();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+//starting app
 app.Run();
