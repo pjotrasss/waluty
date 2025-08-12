4fbc4b7ee6f7b9081bc182c6c6a8edf31ea21b51
pjotrasss <pjotrasss@dom>
2025-08-13 00:38:18 +0200

    db migrations

--- waluty/Migrations/20250812220802_RemoveDefaultExchangeRateTableInit.Designer.cs
+++ waluty/Migrations/20250812220802_RemoveDefaultExchangeRateTableInit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using waluty.Data;
 
@@ -11,9 +12,11 @@ using waluty.Data;
 namespace waluty.Migrations
 {
     [DbContext(typeof(CurrenciesDbContext))]
-    partial class CurrenciesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250812220802_RemoveDefaultExchangeRateTableInit")]
+    partial class RemoveDefaultExchangeRateTableInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
