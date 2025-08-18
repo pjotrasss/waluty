28a4f302cd67a8e56220bd76120e5616a6dc255f
pjotrasss <pjotrasss@dom>
2025-08-18 16:57:50 +0200

    viewmodel for controller-view communication (showing currencies from db)

---
+++ waluty/Models/CurrenciesView.cs
@@ -0,0 +1,9 @@
+﻿namespace waluty.Models
+{
+    public class CurrenciesView
+    {
+        public required List<DateTime> AvailableDates { get; set; }
+        public DateTime? SelectedDate { get; set; }
+        public required List<CurrencyRate> Rates { get; set; }
+    }
+}
