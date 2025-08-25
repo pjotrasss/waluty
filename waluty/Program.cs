0244c251503ad16e130336551526beeffadac8ea
pjotrasss <pjotrasss@WALUTY>
2025-08-25 12:46:04 +0200

    adding services to app & changes in app's settings

--- waluty/Program.cs
+++ waluty/Program.cs
@@ -1,23 +1,39 @@
-//variable for preparing app
-using Microsoft.EntityFrameworkCore;
+//necessary namespaces
 using waluty.Data;
+using waluty.Repositories;
+using waluty.Services;
 
-//adding services to app
 var builder = WebApplication.CreateBuilder(args);
+
 //add controllers to app
 builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
+
 //http client for API calls
 builder.Services.AddHttpClient();
-//context registrating for db
-builder.Services.AddDbContext<CurrenciesDbContext>
-(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("CurrenciesDb"))
-);
+
+//connection Factory
+var connString = builder.Configuration.GetConnectionString("CurrenciesDb")
+    ?? throw new InvalidOperationException("Connection string 'CurrenciesDb' not found.");
+builder.Services.AddSingleton(new DbConnectionFactory(connString));
+
+//repositories
+builder.Services.AddScoped<CurrencyRateRepository>();
+builder.Services.AddScoped<ExchangeRateTableRepository>();
+
+//services
+builder.Services.AddScoped<DbCurrenciesService>();
+builder.Services.AddScoped<ApiCurrenciesService>();
+
+//swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 //building app
 var app = builder.Build();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //starting app
 app.Run();
